7dd53901f9af98f5f1b67463175faf061674c885
Liab08 <lia.bukspan@gmail.com>
2024-12-27 16:50:51 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,46 +3,6 @@ package Explorer;
 		public class Main {
 			public static Scanner reader = new Scanner(System.in); 
 
-
-			public static Node<Integer> f1(Node<Integer> H) {
-				Node<Integer> current = H;
-				Node<Integer> newH = new Node<>(null);
-				Node<Integer> newCurrent = newH;
-				while (current.getNext().getNext()!=null) {
-					newCurrent.setNext(current.getNext());
-					newCurrent = current.getNext();
-					current.setNext(current.getNext().getNext());
-					current = current.getNext();
-				}
-				newCurrent.setNext(current.getNext());
-				current.setNext(current.getNext().getNext());
-				return newH.getNext();
-			}
-		
-
-			public static Node<Integer> f2(Node<Integer> head) {
-			    Node<Integer> current1 = head;
-
-			    while (current1 != null) {
-			        Node<Integer> current2 = current1.getNext();
-			        Node<Integer> previous2 = current1;
-
-			        while (current2 != null) {
-			            if (current2.getValue()==current1.getValue()) {
-			                previous2.setNext(current2.getNext()); 
-			            } else {
-			                previous2 = current2;  
-			            }
-			            current2 = current2.getNext();  
-			        }
-			        current1 = current1.getNext();   
-			    }
-
-				return head;
-			}
-			
-		
-			
 			public static Node<Integer> q1(Node <Integer> list1, Node <Integer>list2) {
 			    Node <Integer> Head = null, c = null;
    
@@ -102,16 +62,238 @@ package Explorer;
 
 			    return head;
 			}
+			
+
+			public static int q3(Node<Integer> a1, int n){
+				Node<Integer> a = a1;
+				int i=0;
+				int length=0;
+
+				 while (a!= null) {
+			            length++;
+			            a =a.getNext();
+			        }
+				 a=a1;
+				 
+				 int minl=length;
+				 int minr=length;
+				 
+				 while(a!=null) {
+					if(a.getValue()==n) {
+						if(i<minl) {
+							minl=i;
+							
+						}
+						if(length-i-1<minr) {
+							minr=length-i-1;
+						}
+					}
+					i++;
+					a=a.getNext();
+				}
+				 return minr+minl;
+				
+			}
+			
+			
+			 public static boolean q4(Node<Integer>head) {
+				 Node<Integer> list = head;
+				 Node<Integer> current = head.getNext();
+				 while(list!=null) {
+					 
+					 while(current!=null) {
+						 if(list.getValue()==current.getValue()) {
+							 return false;
+						 }
+						 else {
+							 current=current.getNext();
+						 }
+					 }
+					 list=list.getNext();
+				 }
+			return true;	 
+			 }
+			 
+			public static Node q5(Node<Integer>L1) {
+				    Node<Integer> head = L1;
+				    Node<Integer> current1 = L1;
+				    Node<Integer> current2 = L1;
+
+
+				    while (current1.hasNext()) {
+				    	current2=current1;
+					    while (current2.hasNext()) {
+					    	if(current1.getValue()==current2.getNext().getValue()) {
+					    		current2.setNext(current2.getNext().getNext());
+					    	}
+					    	else {
+					    		current2=current2.getNext();
+					    	}
+					    	
+					    }
+			    		current1=current1.getNext();
+
+				    	
+				    }
+			    	return head;
+
+				}
+				
+			 public static int q6(Node<Integer> L1) {
+			        Node<Integer> head = L1;
+			        int counter = 1;
+			        int max = 0;
+
+			        while (head.getNext() != null) {
+			            if (head.getNext().getValue() >= head.getValue()) {
+			                counter++;
+			                head = head.getNext();
+			            } else {
+			            	if(counter>max) {
+			            		max=counter;
+			            	}
+			                counter = 1;
+			                head = head.getNext();
+			            }
+			        }
+			        if(counter>max) {
+			        	max=counter;
+			        }
+			        return max;
+			    }
+			 
+			 
+			 public static void q7(Node<Integer> L1) {
+				    Node<Integer> head = L1;
+				    Node<Integer> maxPointer = head;
+				    Node<Integer> startMax = head;
+				    Node<Integer> startCur= head;
+				    int counter = 1;
+				    int max = 0;
+
+				    while (head.getNext() != null) {
+				        if (head.getNext().getValue() >= head.getValue()) {
+				            counter++;
+				            head = head.getNext();
+				        } 
+				        else {
+				            if (counter > max) {
+				                max = counter;
+				                startMax = startCur;
+				            }
+				            counter = 1;
+				            head = head.getNext();
+				            startCur = head;
+				        }
+				    }
+				    if (counter > max) {
+				        max = counter;
+				        startMax = startCur;
+				    }
+
+				    Node<Integer> temp = startMax;
+				    for (int i = 0; i < max; i++) {
+				        System.out.print(temp.getValue() + " ");
+				        System.out.println();
+				        temp = temp.getNext();
+				    }
+				}
+			 
+
+			
+			
+			public static boolean q8(Node<Integer> a1,Node<Integer> b1){
+				Node<Integer> a = a1;
+		        Node<Integer> b = b1;
+				while(a!=null) {
+					if(b==null) {
+						b=b1;
+					}
+					if(a.getValue()==b.getValue()) {
+						b=b.getNext();
+					}
+					else {
+						return false;
+					}
+					a=a.getNext();
+				}
+				if(b==null) {
+					return true;
+				}
+				return false;
+				
+			}
+			
+			
+			public static boolean q9(Node<Integer> a1,Node<Integer> b1){
+				Node<Integer> a = a1;
+		        Node<Integer> b = b1;
+				while(a!=null) {
+					if(b==null) {
+						return true;
+					}
+					if(a.getValue()==b.getValue()) {
+						b=b.getNext();
+					}
+					else {
+						b=b1;
+					}
+					a=a.getNext();
+				}
+				if(b==null) {
+					return true;
+				}
+				return false;
+			}
+			
+			
+			
+			public static boolean q10(Node<Integer> a1){
+				int [] arr= {0,0,0,0,0,0,0,0,0,0,0};
+				while(a1!=null) {
+					if(a1.getValue()>10 && a1.getValue()<1) {
+						arr[a1.getValue()-1]++;
+					}
+					a1=a1.getNext();
+				}
+				for(int i=0;i<10;i++) {
+					if(arr[i]==0) {
+						return false;
+					}
+				}
+				return true;
+			}
+			
+			
+			public static boolean f10(Node<Integer> a1){
+				Node<Integer> a = a1;
+				boolean b=false;
+				for(int i=1;i<11;i++) {
+					b=false;
+					while(a1!=null) {
+						if(i==a1.getValue()) {
+							b=true;
+						}
+						a1=a1.getNext();
+				}
+					a1=a;
+					if(b==false) {
+						return false;
+					}
+				}
+				return true;
+			
+			}
 
 			
 			
 		    public static void main(String[] args) {
-		        Node<Integer> node1 = new Node<>(-8);
-		        Node<Integer> node2 = new Node<>(-6);
-		        Node<Integer> node3 = new Node<>(-1);
+		        Node<Integer> node1 = new Node<>(1);
+		        Node<Integer> node2 = new Node<>(3);
+		        Node<Integer> node3 = new Node<>(5);
 		        Node<Integer> node4 = new Node<>(1);
-		        Node<Integer> node5 = new Node<>(4);
-		        Node<Integer> node6 = new Node<>(6);
+		        Node<Integer> node5 = new Node<>(1);
+		        Node<Integer> node6 = new Node<>(8);
 
 		        node1.setNext(node2);
 		        node2.setNext(node3);
@@ -122,17 +304,13 @@ package Explorer;
 		        Node<Integer> n1 = new Node<>(1);
 		        Node<Integer> n2 = new Node<>(3);
 		        Node<Integer> n3 = new Node<>(5);
-		        Node<Integer> n4 = new Node<>(8);
-		        Node<Integer> n5 = new Node<>(10);
-		        Node<Integer> n6 = new Node<>(11);
+		      
 
 		        n1.setNext(n2);
 		        n2.setNext(n3);
-		        n3.setNext(n4);
-		        n4.setNext(n5);
-		        n5.setNext(n6);
+		  
 		        
-		        System.out.println(q1(node1,n1));
+		        System.out.println(q3(node1,5));
 		
 		      }
 
